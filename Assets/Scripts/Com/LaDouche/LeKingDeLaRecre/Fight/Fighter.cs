c3255a05aae29630254a5820c5f2e88416d18ffd
lorri <29197383+ZwenDo@users.noreply.github.com>
2022-02-19 01:53:40 +0100

    Added missing files

---
+++ Assets/Scripts/Com/LaDouche/LeKingDeLaRecre/Fight/Fighter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Com.LaDouche.LeKingDeLaRecre.Fight
+{
+    public abstract class Fighter : MonoBehaviour
+    {
+        [SerializeField] private Stats _stats;
+        [SerializeField] private string _name;
+        [SerializeField] protected List<Attack> _attacks;
+        public Stats Stats => _stats;
+
+        public bool IsGud => _stats.Hp > 0;
+
+        public void OnTatkZebi(Attack attack)
+        {
+            _stats.Hp -= attack.Damage;
+        }
+        
+        
+        public abstract Attack NextAttack();
+    }
+}
